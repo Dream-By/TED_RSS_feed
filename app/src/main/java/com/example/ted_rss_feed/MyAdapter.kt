181da084223f8af4495e294fa59b3fb6ae30cdfa
Dream-By <dzmitry.dzianisau@gmail.com>
2020-08-18 13:06:25 +0300

    add new features

--- app/src/main/java/com/example/ted_rss_feed/MyAdapter.kt
+++ app/src/main/java/com/example/ted_rss_feed/MyAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import org.w3c.dom.Text
 
 class MyAdapter(val items: List<Item>): RecyclerView.Adapter<ItemsViewHolder>() {
 
@@ -17,14 +18,14 @@ class MyAdapter(val items: List<Item>): RecyclerView.Adapter<ItemsViewHolder>() 
 
     override fun getItemCount(): Int {
         return items.size
-        System.out.println("items.size - " + items.size)
+
     }
 
     override fun onBindViewHolder(holder: ItemsViewHolder, position: Int) {
         val title = items[position].title ?: ""
-
         val imageurl = items[position].image.url ?: ""
-        holder.bind(title, imageurl)
+        val timing:String = items[position].duration.text ?:""
+        holder.bind(title, imageurl,timing)
     }
 }
 
@@ -33,12 +34,14 @@ class ItemsViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
     private val photo: ImageView = itemView.findViewById(R.id.image_photo)
     private val title_text: TextView = itemView.findViewById(R.id.title_)
     private val description_text:TextView = itemView.findViewById(R.id.description)
+    private val timing_text:TextView = itemView.findViewById(R.id.textTiming)
 
 
-    fun bind(title: String,imageurl:String) {
+    fun bind(title: String,imageurl:String,timing:String) {
         Glide.with(itemView.context).load(imageurl).into(photo)
         title_text.text = title.substringAfter('|').drop(1)
         description_text.text = title.substringBefore('|').dropLast(1)
+        timing_text.text = timing.substringAfter(":")
     }
 
 }
